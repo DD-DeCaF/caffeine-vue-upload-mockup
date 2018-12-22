c2e84e57f8fe1574335a1d51877971eb8f67b82b
Ali Kaafarani <ali@kvikshaug.no>
2018-12-22 23:00:30 +0100

    chore: install vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blue.lighten1,
+  },
+});
 
 new Vue({
   router,
