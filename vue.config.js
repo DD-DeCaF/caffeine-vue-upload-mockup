95c6d091ffebc12ba212f34dc9e57adb92e3984e
Ali Kaafarani <ali@kvikshaug.no>
2019-01-07 16:52:50 +0100

    chore: temporarily deal with webpack bug

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   lintOnSave: false,
+  devServer: {
+    // Temporarily deal with https://github.com/webpack/webpack-dev-server/issues/1604
+    disableHostCheck: true,
+  }
 };
